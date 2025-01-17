1e2eb7216162f9fc5d0d6d90ac5a1423c338363e
r.fairlie <git@chaoticgoose.com>
2025-01-17 23:58:55 +1100

    feat: gradient code

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,24 @@
 #include <iostream>
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+    // Image
+    int width = 256;
+    int height = 256;
+
+    // Render 
+    std::cout << "P3\n" << width << ' ' << height << "\n255\n";
+
+    for (int j = 0; j < height; j++) {
+        for (int i = 0; i < width; i++) {
+            auto r = double(i) / (width-1);
+            auto g = double(j) / (height-1);
+            auto b = 0.0;
+
+            int ir = int(255.999 * r);
+            int ig = int(255.999 * g);
+            int ib = int(255.999 * b);
+
+            std::cout << ir << ' ' << ig << ' ' << ib << "\n";
+        }
+    }
 }
